5cb79cc553ce817e2c5a2f79e8b582f090c2dd04
Karisha:) <krshmelendez@gmail.com>
2025-06-06 12:50:19 +0200

    arreglar los paths en router y app

--- src/components/backButton/BackButton.jsx
+++ src/components/backButton/BackButton.jsx
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
-import BasicButton from '../BasicButton/BasicButton';
+import BasicButton from '../basicButton/BasicButton';
 import { Link } from "react-router-dom";
 
 function BackButton(){
