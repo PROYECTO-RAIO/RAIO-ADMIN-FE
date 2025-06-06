96f00a65fdb7f41d93466b7123073acf3e255895
kat-lev <kat@katleverton.com>
2025-06-06 12:06:36 +0200

    Crear botón de Volver

---
+++ src/components/backButton/BackButton.jsx
@@ -0,0 +1,28 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+import BasicButton from '../BasicButton/BasicButton';
+import { Link } from "react-router-dom";
+
+function BackButton(){
+
+    return(
+        <>
+        <Navbar>
+        <Container>
+          <Nav className="navbar">
+            <BasicButton 
+                className={"btn-primary-custom"}
+                as={Link} 
+                to="/categorias">
+                volver
+            </BasicButton>
+          </Nav>
+        </Container>
+        </Navbar>
+        </>
+    )
+}
+
+export default BackButton;
--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,6 +1,9 @@
+import BackButton from "../components/backButton/BackButton"; 
+
 function Create() {
     return(
         <>
+        <BackButton />
         <h1>Crear categorías</h1>
         </>
     )
